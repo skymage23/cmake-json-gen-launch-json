52e90d00943f7d0eb826a2877d89bcb2c9c0a54a
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-02-11 15:15:10 -0600

    Added  "scripts/os_host_setup.cmake", which contains os-specific definitons of "echo_parameter_using_shell", which uses the system shell to echo its input to stdout.  This is necessary for some planned CMake jank (admittedly) hackery.

---
+++ scripts/gen_launch_json.cmake
@@ -0,0 +1,34 @@
+include_guard(GLOBAL)
+set(PROJECT_BASE "${CMAKE_SOURCE_DIR}") #This SHOULD be set to the current project base
+include(${CMAKE_CURRENT_LIST_DIR}/../third_party/cmake-build-sys-utilities/scripts/build_sys_opts.cmake)
+include(os_host_setup.cmake)
+#What do we need?
+#We need the target name and the path to the binary.
+#We also need to differentiate between test and release binaries.
+#For test binaries, we need to make sure we accurately set the debugger.
+
+#function(get_target_binary_path TARGET)
+
+#Need to do a Windows check.
+#set()
+
+#endfunction()
+set(ECHO_OUTPUT)
+echo_parameter_using_shell("Hello, world")
+message(STATUS "${ECHO_OUTPUT}")
+#function(get_target_arguments)
+#    set(TARGETS "")
+#    set(TARGET_PATHS "")
+#    set(TARGET_TYPES "")
+#
+#    get_targets_list_under_dir(${PROJECT_BASE})
+#    set(TARGETS "${OUTPUT_TARGET_TEMP}")
+#
+#    foreach(TARGET in "${TARGETS}")
+#       get_target_property()
+#    endforeach()
+#endfunction()
+
+#function(gen_launch_json_script)
+    #Hello:
+#endfunction(gen_launch_json_script)
---
+++ scripts/os_host_setup.cmake
@@ -0,0 +1,87 @@
+include_guard(GLOBAL)
+
+#Universal dependencies:
+find_package(Python3)
+#preliminary universal functions
+
+function(find_dependency_program PROGRAM_NAME)
+    find_program(PROGRAM_FOUND ${PROGRAM_NAME})
+    if(PROGRAM_FOUND)
+        set(PROGRAM_FOUND TRUE PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(look_for_dependency_program)
+    unset(FOUND_ALTERNATIVE PARENT_SCOPE)
+    list(LENGTH ARGN ARG_LEN_OUTPUT)
+    if(ARG_LEN_OUTPUT LESS 1)
+        message(FATAL_ERROR "No arguments passed.")
+    endif()
+    unset(FOUND_ALTERNATIVE)
+    foreach(ALTERNATIVE ${ARGN})
+        #find_dependency_program(${ALTERNATIVE})
+	find_program(PROGRAM_FOUND ${ALTERNATIVE})
+        if(PROGRAM_FOUND)
+	    set(FOUND_ALTERNATIVE "${PROGRAM_FOUND}" PARENT_SCOPE)
+    	    return()
+        endif()
+    endforeach()
+endfunction()
+
+#Host OS specific setup:
+if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+    #Dependencies
+    look_for_dependency_program("powershell" "pwsh")
+    if(NOT FOUND_ALTERNATIVE)
+        message(FATAL_ERROR "PowerShell not found.")
+    endif()
+    set(POWERSHELL_COMMAND ${FOUND_ALTERNATIVE})
+
+    function(echo_parameter_using_shell INPUT)
+        set(ECHO_OUTPUT "")
+        execute_process(
+            COMMAND "${POWERSHELL_COMMAND}" "-c" "\"Write-Host '${INPUT}'\""
+            OUTPUT_VARIABLE ECHO_OUTPUT
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+        set(ECHO_OUTPUT "${ECHO_OUTPUT}" PARENT_SCOPE)
+    endfunction()
+
+elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin")
+
+    look_for_dependency_program("sh")
+    if(NOT FOUND_ALTERNATIVE)
+        message(FATAL_ERROR "\"sh\" not found. Did you make changes to your PATH variable?")
+    endif()
+
+    function(echo_parameter_using_shell INPUT)
+        set(ECHO_OUTPUT "")
+        execute_process(
+            COMMAND "sh" "-c" "\"echo '${INPUT}'\""
+            OUTPUT_VARIABLE ECHO_OUTPUT
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+        set(ECHO_OUTPUT "${ECHO_OUTPUT}" PARENT_SCOPE)
+    endfunction()
+
+    elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
+
+    look_for_dependency_program("echo")
+    if(NOT FOUND_ALTERNATIVE)
+        message(FATAL_ERROR "\"echo\" is not on the system PATH. Did you make changes to your PATH variable, or is this not a typical Linux distro.")
+    endif()
+
+
+    function(echo_parameter_using_shell INPUT)
+        set(ECHO_OUTPUT "")
+        execute_process(
+            COMMAND "echo"  "'${INPUT}'"
+            OUTPUT_VARIABLE ECHO_OUTPUT
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+        set(ECHO_OUTPUT "${ECHO_OUTPUT}" PARENT_SCOPE)
+    endfunction()
+
+else()
+    message(FATAL_ERROR "\"${CMAKE_HOST_SYSTEM_NAME}\" is not a supported build host OS.")
+endif()
