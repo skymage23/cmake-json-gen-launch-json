25b047f467cbb92d7fa8a7dfe736506ea3232581
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-03-17 16:36:55 -0500

    Updated "gen_sample_output_file.sh" to try to force a switch to the US english UTF-8 locale if it is installed.  Also fixed the script so that it generated output matchine "gen_sample_output_file.ps1', and, thereby, getting the SHA1-oriented Python tests to pass on a SystemD-based Linux distro (Fedora).

--- tests/python_tests/test_helpers/test_sha1_hash_args/gen_sample_output_file.sh
+++ tests/python_tests/test_helpers/test_sha1_hash_args/gen_sample_output_file.sh
@@ -1,9 +1,43 @@
 #!/bin/sh
 #LOCALE
+
+function print_err {
+    echo >&2 "$1"
+}
+
+function die {
+    print_err $1
+    exit 1
+}
+
+#Try to force UTF-8 with no BOM
+old_LOCALE=""
+UTF8_LOCALE="en_US.UTF-8"
+UTF8_LOCALE_REGEX="en_US\.UTF-8"
+
+if [ "$(echo "$LANG" | awk -c '$1~/UTF-8/ {print $1}')" == "" ]; then
+    echo "We need to try to change the locale."
+    if which localectl 2>&1 >/dev/null; then
+        if ! `sudo localectl list-locales | awk -c "\$1~/$UTF8_LOCALE_REGEX/{print \$1}"`; then
+            die "The $UTF8_LOCALE locale is not installed or configured on this system."            
+        fi
+
+        old_locale="$LANG"
+        ! sudo localectl set-locale "UTF8_LOCALE" && die "Unable to set locale to UTF8_LOCALE"
+
+    else
+        echo "This is not a SystemD-based distro. We will TRY our best"
+        echo "to ensure this script works correctly, but we cannot guarantee it."
+        echo "Welcome to the world of UNIX-like OSs, where developer ideological"
+        echo "differences manifest as imcompatibilities."
+
+        old_locale="$LANG"
+        export LANG="$UTF8_LOCALE"
+    fi
+fi
+
 ##Locate project base:
-echo "this_file: $0"
 script_dir="$(realpath $(dirname $0))"
-echo "script_dir: $script_dir"
 #Import development-shell-helpers for POSIX shells:
 dir="$script_dir"
 while [ ! -e "$dir/LICENSE" ]; do
@@ -36,14 +70,25 @@ TEMP_FILE_PATH="$(mktemp $TEMP_FILE_NAME)"
     
 
 #Read in list of targets, sort it, and convert uppercase to lowercase:
-cat $SAMPLE_TARGET_LIST_FILE | sort | tr '[:upper:]' '[:lower:]' > $TEMP_FILE_PATH
+PRINT_PROCESSED_INPUT="$(cat $SAMPLE_TARGET_LIST_FILE | tr '[:upper:]' '[:lower:]' | sort)"
+echo "$PRINT_PROCESSED_INPUT" | tr -d \\n > $TEMP_FILE_PATH
 
 #Calculate the hash:
 HASH="$(sha1sum $TEMP_FILE_PATH | awk '{print $1}')"
 
 #Write hash and sorted list to output file
 echo "$HASH" > $SAMPLE_FILE_NAME
-cat $TEMP_FILE_PATH >> $SAMPLE_FILE_NAME
+echo "$PRINT_PROCESSED_INPUT" >> $SAMPLE_FILE_NAME
 
 #Remove temp file:
 rm -rf $TEMP_FILE_PATH
+
+
+if [ "$old_LOCALE" != "" ] ; then 
+    if which localectl 2>&1 >/dev/null; then
+        ! sudo localectl set-locale "$old_LOCALE" && die "Unable to set locale to back to $old_LOCALE"
+
+    else
+        export LANG="$old_LOCALE"
+    fi
+fi
